5e819a2ad3527f2f6e7d3cd38c7360596f095222
Ahasan Ullah <ahasanullah451@gmail.com>
2024-09-19 11:52:37 +0600

    home page added

--- script/login.js
+++ script/login.js
@@ -1,6 +1,14 @@
 document.getElementById('login-btn').addEventListener('click',function(event){
   event.preventDefault();
 
-  const phoneNumber=document.getElementById('phn-field');
-  const pin=document.getElementById('pin-field');
+  const phoneNumber=document.getElementById('phn-field').value;
+  const pin=document.getElementById('pin-field').value;
+
+  // temp
+  if(phoneNumber==='5' && pin==='2'){
+    window.location.href='/home.html'
+  }
+  else{
+    alert('Wrong pin or pass');
+  }
 })
